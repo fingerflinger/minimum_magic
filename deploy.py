3dc998511b1a7730dde2e98b83534edb8d650fb0
Eric Vaughan <ericlvaughan@gmail.com>
2022-06-26 13:34:02 -0600

    Mkdir init Scrython

--- deploy.py
+++ deploy.py
@@ -3,24 +3,25 @@ import os
 
 asset_path = "assets"
 
+
+# Let's download some card images and put them into an assets dir
 def main():
-    # Let's download some card images and put them into an assets dir
-    # If folder exists
-    if os.path.isfile(asset_path):
-        pass
-    # else, make the folder
-    else:
-        import pdb;pdb.set_trace()
+    # If folder doesn't exist
+    import pdb;pdb.set_trace()
+    if not os.path.exists(os.path.join(os.getcwd(), asset_path)):
         os.mkdir(asset_path)
     
     # For each file on list, check for exsistence. If not present, then download.
     # After downloading or if exsists already, compare hash
 
+    download_card("Mountain")
 
-def download_card():
-    pass
+def download_card(card_def):
+    # TODO define card by scryfall id, but how to get the id of versions I actually want to use?
+    card = scrython.cards.Named(fuzzy=card_def)
+
+# hash the existing files for later comparison
 def snapshot():
-    # hash the existing files for later comparison
     pass
 
 if __name__ == "__main__":
