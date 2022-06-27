ca4e1882f8c38b71730854d95570a24b3c1b6026
Eric Vaughan <ericlvaughan@gmail.com>
2022-06-27 13:21:38 -0600

    Plink away at deploy features. Can download with no error checking or name formatting. Need to make more robust and test harnesses

--- deploy.py
+++ deploy.py
@@ -1,28 +1,73 @@
 import scrython
 import os
+import urllib.request
+import hashlib
 
 asset_path = "assets"
 
+card_list = [("Mountain", "e1af2d09-908e-442e-a9d0-9c618a71a327")] # Mountain
+
+
+def generate_manifest(card_list, asset_path):
+    # TODO
+    # generate a manifest that enables us to check our environment is set up correctly 
+    pass
+
+def find_card(card_name, set_code, artist):
+    query = "name:{}".format(card_name)
+    if set_code:
+        query += " set:{}".format(set_code)
+    if artist:
+        query += " artist:{}".format(artist)
+
+    results = scrython.cards.Search(q=query)
+    if results.card_count() == 1:
+        return results
+    else:
+        return False # Err code too?
+
+def hash_file(file_loc):
+    pass
+
+def check_file(file_loc):
+    if(os.path.isfile(file_loc)): 
+        return hash_file(file_loc)
+    return False
+
+def do_download(uri, destination, verification_hash=""):
+    if(verification_hash):
+        check_file(destination, verification_hash)
+    urllib.request.urlretrieve(uri, destination)
+    # TODO check for file
+
+def construct_file_name(card_name, suffix):
+    # TODO
+    # Need to handle spaces and tricky characters in the card names
+    return card_name + "_small.jpg"
+
+def download_card(card_id, destination, verification_hash):
+    card = scrython.cards.Id(id=card_id)
+    my_links = card.image_uris()
+   
+    if ("small" in my_links.keys()):
+        do_download(my_links["small"], "assets/" + construct_file_name(card.name(), "_small.jpg"))
+    
+    if ("normal" in my_links.keys()):
+        do_download(my_links["normal"], "assets/" + construct_file_name(card.name(), "_normal.jpg"))
+
+# hash the existing files for later comparison
+def snapshot():
+    pass
 
 # Let's download some card images and put them into an assets dir
 def main():
     # If folder doesn't exist
-    import pdb;pdb.set_trace()
     if not os.path.exists(os.path.join(os.getcwd(), asset_path)):
         os.mkdir(asset_path)
     
     # For each file on list, check for exsistence. If not present, then download.
     # After downloading or if exsists already, compare hash
-
-    download_card("Mountain")
-
-def download_card(card_def):
-    # TODO define card by scryfall id, but how to get the id of versions I actually want to use?
-    card = scrython.cards.Named(fuzzy=card_def)
-
-# hash the existing files for later comparison
-def snapshot():
-    pass
+    download_card(card_list[0], asset_path)
 
 if __name__ == "__main__":
     main()
